e6507587776cae404e8be46d1544b611707fd887
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 15:52:47 +0530

    Add files via upload

---
+++ pro11.php
@@ -0,0 +1,17 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+        <title>php example</title>
+</head>
+<body>
+    <?php
+         // display a heading
+         echo "<h1>welcome to my webpage</h1>"
+              
+         // display a paragraph
+         echo "<p> this is a paragraph
+displayed using php. </p>";
+?>
+</body>
+</html> 
+
