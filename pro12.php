ae0b823265315adf9ff38cc83f84be6c6ba2969b
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 15:54:52 +0530

    Add files via upload

---
+++ pro12.php
@@ -0,0 +1,13 @@
+ <?php
+ // define two integer variables
+ $num1=10;
+ $num2=20;
+
+ // display both numbers
+ echo "first number: " . $num1 . "<br>";
+ echo "second number: " . $num2 . "<br>";
+
+ // calculate and display the sum
+ $sum= $sum1 + $sum2;
+ echo "sum of both numbers: " . $sum;
+ ?>
