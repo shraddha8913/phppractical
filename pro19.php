81cffcc557835ff72028388c9476bc9cfd191089
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 16:04:58 +0530

    Add files via upload

---
+++ pro19.php
@@ -0,0 +1,58 @@
+<?php
+  echo "<table border='1'; border-collapse='collapse>";
+  echo "<b'>";
+  echo "<tr>";
+  echo "<td colspan='3'; text-align='center'> Name </td>" ; 
+  echo "</tr>";
+    echo "<tr>";
+    echo "<td> Subject Name </td>";
+    echo "<td> Max Marks  </td>";
+    echo "<td> Obtained Marks  </td>";
+      echo "</tr>";
+          echo "<tr>";
+    echo "<td> sub1 </td>";
+    echo "<td> 50  </td>";
+    echo "<td> 40  </td>";
+      echo "</tr>";
+           echo "<tr>";
+    echo "<td> sub2 </td>";
+    echo "<td> 50  </td>";   
+    echo "<td> 30  </td>";
+      echo "</tr>";
+            echo "<tr>";
+    echo "<td> sub3 </td>";
+    echo "<td> 50  </td>";
+    echo "<td> 45  </td>";
+      echo "</tr>";
+                  echo "<tr>";
+    echo "<td> sub4 </td>";
+    echo "<td> 50  </td>";
+    echo "<td> 48  </td>";
+      echo "</tr>";
+           echo "<tr>";
+    echo "<td> sub5</td>";
+    echo "<td> 50  </td>";
+    echo "<td> 34  </td>";
+      echo "</tr>";
+         echo "<tr>";
+    echo "<td> Total</td>";
+    echo "<td> 250  </td>";
+    echo "<td> 197  </td>";
+      echo "</tr>";
+        echo "<tr>";
+    echo "<td> Percentage</td>";
+    echo "<td colspan='2'> 78.8  </td>";
+   
+      echo "</tr>";
+        echo "<tr>";
+    echo "<td> Result</td>";
+    echo "<td> passed/fail  </td>";
+    echo "<td> With Distinction  </td>";
+      echo "</tr>";
+
+
+
+
+
+
+?>
