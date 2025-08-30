dc1283ba9560c0d900f715d2ea780505af110571
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 15:55:39 +0530

    Add files via upload

---
+++ pro14.php
@@ -0,0 +1,19 @@
+<?php
+//define three numbers
+$a = 10;
+$b = 20;
+$c = 30;
+// caculate sum
+$sum = $a + $b + $c;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>   
+    <title>sum of three nummbers</title>
+</head>
+<body>
+    <h2>sum of three numbers</h2>
+    <p>the sum of <?= $a ?>, <?= $b ?> and <?= $c ?> is: <b><?= $sum ?></b></p>
+</body>
+</html>       
