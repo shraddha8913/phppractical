619b306c22cd53cb3d8e67cb7f32af2af4492062
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 16:03:52 +0530

    Add files via upload

---
+++ pro16.php
@@ -0,0 +1,26 @@
+<?php
+       echo "printing numbers using for loop";
+       echo "<br>";
+       for($i=1; $i<=20; $i++){
+        echo "$i";
+         echo "<br>";
+       }
+        echo "printing numbers using while loop";
+       echo "<br>";
+       $i=1;
+       while($i<=20){
+        echo "$i";
+        $i++;
+        echo "<br>";
+       }
+        echo "printing numbers using do while loop";
+       echo "<br>";
+       $i=1;
+       do{
+        echo "$i";
+        $i++;
+         echo "<br>";
+       }while($i<=20)
+
+
+?>
