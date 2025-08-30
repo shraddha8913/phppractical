d198c66b3b923e3e2a401a71bb50cc61c4e35113
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 16:05:18 +0530

    Add files via upload

---
+++ pro20.php
@@ -0,0 +1,27 @@
+<?php
+$arr=[1,2,3,4,5,6,7,8,9,10];
+  echo "Number of first index is  $arr[0]";
+  echo "<br>";
+    echo "Number of second index is  $arr[1]";
+  echo "<br>";
+    echo "Number of third index is  $arr[2]";
+  echo "<br>";
+    echo "Number of fourth index is  $arr[3]";
+  echo "<br>";
+    echo "Number of fifth index is  $arr[4]";
+  echo "<br>";
+    echo "Number of sixth index is  $arr[5]";
+  echo "<br>";
+    echo "Number of seventh index is  $arr[6]";
+  echo "<br>";
+    echo "Number of eighth index is  $arr[7]";
+  echo "<br>";
+    echo "Number of ninth index is  $arr[8]";
+  echo "<br>";
+    echo "Number of tenth index is  $arr[9]";
+  echo "<br>";
+
+   
+
+
+?>
