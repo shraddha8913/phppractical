3c949826d7b7a0ec4944fbeae191e0111a7dc9ed
shraddha8913 <dubeyshraddha107@gmail.com>
2025-08-30 16:03:30 +0530

    Add files via upload

---
+++ pro15.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Paragraph with CSS using PHP</title>
+</head>
+<body>
+    <?php
+        // PHP outputs HTML with CSS styling
+        echo "<p style='color:green; font-size:20px;'>This is a paragraph with green text styled using CSS inside PHP.</p>";
+    ?>
+</body>
+</html>
