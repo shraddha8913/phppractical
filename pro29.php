be7532518f237de381bed55014acc1f3ae294724
shraddha8913 <dubeyshraddha107@gmail.com>
2025-09-03 14:46:20 +0530

    Add files via upload

---
+++ pro29.php
@@ -0,0 +1,12 @@
+<?php
+    $arr=[1,2,3,4,5,5,5,5,];
+    $new=array_unique($arr);
+
+    echo "original array=";
+    print_r($arr);
+       echo "<br>";
+
+     echo "new array=";
+    print_r($new);
+
+?>
