5ecdd8756cca06e68554ed88d12b41710e102dbe
shraddha8913 <dubeyshraddha107@gmail.com>
2025-09-03 14:48:06 +0530

    Add files via upload

---
+++ pro30.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr=["India"=>"New Delhi", "Pakistan"=>"Islamabad", "Japan"=>"Tokyo","China"=>"Beijing","Saudi Arabia"=>"Riayad"];
+
+
+foreach($arr as $key=>$value){
+    echo "<pre>";
+       echo "key=$key"."value=$value";
+        echo "</pre>";
+}
+
+
+?>
