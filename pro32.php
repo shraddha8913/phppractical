caf4220f8d9d66fce78ea3350383b2557a6372d3
shraddha8913 <dubeyshraddha107@gmail.com>
2025-09-04 19:42:01 +0530

    Add files via upload

---
+++ pro32.php
@@ -0,0 +1,15 @@
+<?php
+// Create an associative array of students and their marks
+$students = array(
+    "shraddha" => 85,
+    "falguni" => 92,
+    "shiba"  => 78,
+    
+);
+
+// Update one student's marks
+$students["shiba"] = 90; // updating Amit's marks
+
+// Print the updated array
+print_r($students);
+?>
