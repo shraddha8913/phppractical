f5e786a8c4d3d372114fdda0b1aca4cecef8618c
shraddha8913 <dubeyshraddha107@gmail.com>
2025-09-24 14:25:29 +0530

    Add files via upload

---
+++ pro46.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> <form action="">
+     Name: <input type="text" name="name" id=""> <br> <br>
+      Email: <input type="text" name="subject" id=""><br> <br>
+      Message: <textarea name="messages" id="" rows="3"></textarea> <br> <br>
+   <input type="submit" value="submit">
+</form>
+</body>
+</html>
+
+<?php
+  $name=$_GET['name'];
+  echo " Thank you $name we will contact you soon!";
+?>
